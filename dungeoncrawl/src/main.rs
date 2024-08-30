c9a96f4019a027fa2314c7e15b6a06414e437779
Biagio Liberto <biagioliberto.dev@gmail.com>
2024-08-30 22:09:01 +0200

    add event for player and monsters moves

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,3 +1,5 @@
+use prelude::*;
+
 mod map;
 mod map_builder;
 mod camera;
@@ -9,24 +11,22 @@ mod turn_state;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub use legion::*;
-    pub use legion::world::SubWorld;
     pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+
+    pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+    pub use crate::turn_state::*;
 
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
-    pub use crate::camera::*;
-    pub use crate::components::*;
-    pub use crate::spawner::*;
-    pub use crate::systems::*;
-    pub use crate::turn_state::*;
 }
-
-use prelude::*;
 
 fn main() -> BError {
     let context = BTermBuilder::new()
